99e656d13617dda4c5fde063bdd751bd03730fa3
yusalz <84872458+yusalz@users.noreply.github.com>
2021-12-21 12:25:50 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import appSrc from './app.js';
+import fs from 'fs';
+import express from 'express';
+import bodyParser from 'body-parser';
+import m from 'mongoose';
+import crypto from 'crypto';
+import http from 'http';
+import CORS from './CORS.js';
+import UserModel from './models/User.js';
+const User = UserModel(m);
+const app = appSrc(express, bodyParser, fs, crypto, http, CORS, User, m);
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
